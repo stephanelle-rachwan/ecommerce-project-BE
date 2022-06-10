82a630e480c5d7aff369ff51f615e2daa9a59338
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-10 11:21:42 +0300

    creating the controllers and linking to models | Laravel

--- laravel_server/routes/api.php
+++ laravel_server/routes/api.php
@@ -3,14 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
-// Route::group(['middleware' => 'api'], function($router) {
-//     Route::post('/register', [JWTController::class, 'register']);
-//     Route::post('/login', [JWTController::class, 'login']);
-//     Route::post('/logout', [JWTController::class, 'logout']);
-//     Route::post('/refresh', [JWTController::class, 'refresh']);
-//     Route::post('/profile', [JWTController::class, 'profile']);
-// });
 
 Route::group(['prefix' => 'admin'], function(){
     Route::post('/items', [AdminController::class, "addItems"]); 
