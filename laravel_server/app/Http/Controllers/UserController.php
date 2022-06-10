e7dd02bb3701d3de0558bcef24d1f3081127b05b
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-10 20:13:41 +0300

    creating the apis related to the user | Laravel

--- laravel_server/app/Http/Controllers/UserController.php
+++ laravel_server/app/Http/Controllers/UserController.php
@@ -13,13 +13,26 @@ class UserController extends Controller
     // GET
     public function getItems(Request $request)
     {
-
+        $category_name=$request->id;
+        $items = Category::where('id', '=', $category_name)
+        ->get();
+        return response()->json([
+            "status" => "success",
+            "items" => $items
+        ], 200);
     }
 
     // POST 
     public function likeItems(Request $request)
     {
+        $like = new Like;
+        $like->users_id = $request->users_id;
+        $like->items_id = $request->items_id;
+        $like->save();
 
+        return response()->json([
+            "status" => "Success",
+        ], 200);
     }
 
 }
